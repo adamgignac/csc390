5b7fd5a4628c3a30b861c529c69b8f7de6cfafd7
Adam Gignac <gignac.adam@gmail.com>
2013-11-19 17:48:15 -0500

    Setup file

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(name="Isidore",
+    version='1.0',
+    description='Notetaking for Students',
+    author="Adam Gignac",
+    author_email="gignac.adam@gmail.com",
+)
