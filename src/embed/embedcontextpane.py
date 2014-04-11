9752638f31af9fd6ec1611f10246641bbacd547e
Adam Gignac <gignac.adam@gmail.com>
2014-04-11 02:23:56 -0400

    Oops

---
+++ src/embed/embedcontextpane.py
@@ -0,0 +1,16 @@
+'''
+Created on 2013-12-23
+
+@author: Adam Gignac
+'''
+
+class EmbedContextPane(object):
+    '''
+    Abstract interface for an embed type context pane.
+    All subclasses must implement the getURL() method,
+    which should return the path to the source of the
+    item being embedded.
+    '''
+    
+    def getHtml(self):
+        raise NotImplementedError
